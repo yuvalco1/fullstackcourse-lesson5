dcc50c9410fab61c162ff1d7ef5733d14e14e864
Yuval Cohen <yuvalco1@gmail.com>
2023-11-16 19:29:21 +0200

    start arrays Learn for each loop

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,26 @@ public class Main {
             System.out.println(n);
         }
 
+        String[] cars = {"BMW", "Pegout", "SERES","Ford"};
+        for (int i=0;i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
 
+        int[] numbers = {7, 15, 3, 22, 10, 9, 14, 6};
+        float sum = 0;
+        float average = 0;
+        for (int i=0;i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        average = sum/numbers.length;
+        System.out.println("Average is: "+average);
+        // example for "for each loop"
+        int min = numbers[0];
+        for (int num : numbers){
+            if (num < min ){
+                min = num;
+            }
+        }
+        System.out.println("minimum nuber in array is:"+ min);
     }
 }
